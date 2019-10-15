71699447b75cefb009884115835423654d70e3a5
Kelby Mittan <kelbymittan@Kelbys-MacBook-Pro.local>
2019-10-15 17:19:21 -0400

    Basic steps and map commit

--- Calculator/Calculator/main.swift
+++ Calculator/Calculator/main.swift
@@ -8,23 +8,294 @@
 
 import Foundation
 
+
 func mathStuffFactory(opString: String) -> (Double, Double) -> Double {
-  switch opString {
-  case "+":
-    return {x, y in x + y }
-  case "-":
-    return {x, y in x - y }
-  case "*":
-    return {x, y in x * y }
-  case "/":
-    return {x, y in x / y }
-  default:
-    return {x, y in x + y }
+    
+      switch opString {
+      case "+":
+        return {x, y in x + y }
+      case "-":
+        return {x, y in x - y }
+      case "*":
+        return {x, y in x * y }
+      case "/":
+        return {x, y in x / y }
+      default:
+        return {x, y in x + y }
   }
 }
 
-let closureOperation = mathStuffFactory(opString: "+")
+func customMap(arr: [Double], closure: (Double) -> Double) -> [Double] {
+    var resultArr = [Double]()
+    
+    for num in arr {
+        
+        resultArr.append(closure(num))
+    }
+    
+    return resultArr
+}
 
-let result = closureOperation(45, 5)
+var isCalculating = true
 
-print(result)
+repeat {
+    print()
+    print("Enter Equation")
+    print()
+    let legitOperator = ["+", "-", "*", "/"]
+    let randomOperator = legitOperator.randomElement()
+
+    let userEntry = readLine() ?? ""
+    //let containsChar = legitOperator.joined()
+    var containsChar = " "
+        if userEntry.contains("+") {
+            containsChar = "+"
+        } else if userEntry.contains("-") {
+            containsChar = "-"
+        } else if userEntry.contains("map") {
+            containsChar = "map"
+        } else if userEntry.contains("/") {
+            containsChar = "/"
+        } else if userEntry.contains("?") {
+            containsChar = "?"
+        } else if userEntry.contains("*") {
+            containsChar = "*"
+    }
+    
+    switch containsChar {
+    case "+":
+        let plusArr = userEntry.split(separator: "+")
+        let numOne = Double(plusArr[0])
+        let numTwo = Double(plusArr[1])
+        if let validNumOne = numOne, let validNumTwo = numTwo {
+            let operation = mathStuffFactory(opString: "+")
+            let result = operation(validNumOne, validNumTwo)
+            print()
+            print("Answer: \(validNumOne) + \(validNumTwo) = \(result)")
+            isCalculating = false
+        } else {
+            print("For addition... Please enter like so \"x+y\"!!!")
+        }
+    print(plusArr)
+    case "-":
+        let minusArr = userEntry.split(separator: "-")
+        let numOne = Double(minusArr[0])
+        let numTwo = Double(minusArr[1])
+        if let validNumOne = numOne, let validNumTwo = numTwo {
+            let operation = mathStuffFactory(opString: "-")
+            let result = operation(validNumOne, validNumTwo)
+            print()
+            print("Answer: \(validNumOne) + \(validNumTwo) = \(result)")
+            isCalculating = false
+        } else {
+            print("For addition... Please enter like so \"x+y\"!!!")
+        }
+    case "*":
+        let productArr = userEntry.split(separator: "*")
+        let numOne = Double(productArr[0])
+        let numTwo = Double(productArr[1])
+        if let validNumOne = numOne, let validNumTwo = numTwo {
+            let operation = mathStuffFactory(opString: "*")
+            let result = operation(validNumOne, validNumTwo)
+            print()
+            print("Answer: \(validNumOne) * \(validNumTwo) = \(result)")
+            isCalculating = false
+        } else {
+            print()
+            print("For Multiplication... Please enter like so \"x*y\"!!!")
+        }
+    case "/":
+        let divideArr = userEntry.split(separator: "/")
+        let numOne = Double(divideArr[0])
+        let numTwo = Double(divideArr[1])
+        if let validNumOne = numOne, let validNumTwo = numTwo {
+            let operation = mathStuffFactory(opString: "/")
+            let result = operation(validNumOne, validNumTwo)
+            print()
+            print("Answer: \(validNumOne) / \(validNumTwo) = \(result)")
+            isCalculating = false
+        } else {
+            print()
+            print("For Division... Please enter like so \"x/y\"!!!")
+        }
+    case "?":
+        let punctArr = userEntry.split(separator: "?")
+        let numOne = Double(punctArr[0])
+        let numTwo = Double(punctArr[1])
+        if let validNumOne = numOne, let validNumTwo = numTwo {
+            let operation = mathStuffFactory(opString: randomOperator!)
+            let result = operation(validNumOne, validNumTwo)
+            print()
+            print("Answer: \(validNumOne) ? \(validNumTwo) = \(result)")
+            print()
+            print("Guess what operator was used in this equation!!!")
+            let guessOperator = readLine() ?? ""
+            if guessOperator == randomOperator {
+                print()
+                print("nice job")
+                isCalculating = false
+            } else {
+                print("Oops, it was actually \(randomOperator!)")
+                isCalculating = false
+            }
+        } else {
+            print()
+            print("For Random Operator... Please enter like so \"x?y\"!!!")
+        }
+    case "map":
+        let allElements = userEntry.components(separatedBy: " ")
+        let inputArr = allElements[1].split(separator: ",")
+        var numArrAsDouble = [Double]()
+        let byInt = Double(allElements[4])
+//        let userOperator = allElements[3]
+        for num in inputArr {
+            let dubnum = Double(num)
+            numArrAsDouble.append(dubnum!)
+        }
+        
+        let mappedResult = customMap(arr: numArrAsDouble) { $0 * byInt!}
+        for num in mappedResult {
+            print(num, terminator: ", ")
+        }
+        print()
+        print(customMap(arr: numArrAsDouble) { $0 * byInt!})
+    default:
+        print("please enter equation")
+    }
+    print()
+    print("would you like to try another calculation")
+    let calcAgain = readLine() ?? ""
+    if calcAgain.lowercased() == "yes" {
+        isCalculating = true
+    } else if calcAgain.lowercased() == "no" {
+        isCalculating = false
+        print("thanks for calculating have a nice day")
+    } else {
+        print("Please enter \"yes\" or \"no\"!!!")
+    }
+} while isCalculating == true
+
+//func calculateOperation(str: String) -> Double {
+//   let userEntryArray = str.components(separatedBy: " ") // 2 + 2 => ["2", "+", "2"]
+//   if userEntryArray.count != 3 { // 0, 1, 2
+//       return 0.0
+//   }
+//   let operand1 = Double(userEntryArray[0]) ?? 0.0
+//   let operand2 = Double(userEntryArray[2]) ?? 0.0
+//   let operatorSymbol = userEntryArray[1] //+, -, *, /
+//   let operationClosure = mathStuffFactory(opString: operatorSymbol)
+//   let result = operationClosure(operand1, operand2)
+//   return result
+//}
+//print("calculate")
+//let entryUser = readLine() ?? ""
+//let resultCalculation = calculateOperation(str: entryUser)
+//print(resultCalculation)
+
+//repeat {
+//    print()
+//    print("Enter Equation")
+//    print()
+//    let legitOperator = ["+", "-", "*", "/"]
+//    let randomOperator = legitOperator.randomElement()
+//
+//    let userEntry = readLine() ?? ""
+//
+//    if userEntry.contains("+") {
+//        let plusArr = userEntry.split(separator: "+")
+//        let numOne = Double(plusArr[0])
+//        let numTwo = Double(plusArr[1])
+//        if let validNumOne = numOne, let validNumTwo = numTwo {
+//            let operation = mathStuffFactory(opString: "+")
+//            let result = operation(validNumOne, validNumTwo)
+//            print()
+//            print("Answer: \(validNumOne) + \(validNumTwo) = \(result)")
+//            isCalculating = false
+//        } else {
+//            print("For addition... Please enter like so \"x+y\"!!!")
+//        }
+//    } else if userEntry.contains("-") {
+//        let minusArr = userEntry.split(separator: "-")
+//        let numOne = Double(minusArr[0])
+//        let numTwo = Double(minusArr[1])
+//        if let validNumOne = numOne, let validNumTwo = numTwo {
+//            let operation = mathStuffFactory(opString: "-")
+//            let result = operation(validNumOne, validNumTwo)
+//            print()
+//            print("Answer: \(validNumOne) - \(validNumTwo) = \(result)")
+//            isCalculating = false
+//        } else {
+//            print("For Subtraction.... Please enter like so \"x-y\"!!!")
+//        }
+//
+//    } else if userEntry.contains("*") {
+//        let productArr = userEntry.split(separator: "*")
+//        let numOne = Double(productArr[0])
+//        let numTwo = Double(productArr[1])
+//        if let validNumOne = numOne, let validNumTwo = numTwo {
+//            let operation = mathStuffFactory(opString: "*")
+//            let result = operation(validNumOne, validNumTwo)
+//            print()
+//            print("Answer: \(validNumOne) * \(validNumTwo) = \(result)")
+//            isCalculating = false
+//        } else {
+//            print()
+//            print("For Multiplication... Please enter like so \"x*y\"!!!")
+//        }
+//
+//    } else if userEntry.contains("/") {
+//        let divideArr = userEntry.split(separator: "/")
+//        let numOne = Double(divideArr[0])
+//        let numTwo = Double(divideArr[1])
+//        if let validNumOne = numOne, let validNumTwo = numTwo {
+//            let operation = mathStuffFactory(opString: "/")
+//            let result = operation(validNumOne, validNumTwo)
+//            print()
+//            print("Answer: \(validNumOne) / \(validNumTwo) = \(result)")
+//            isCalculating = false
+//        } else {
+//            print()
+//            print("For Division... Please enter like so \"x/y\"!!!")
+//        }
+//
+//    } else if userEntry.contains("?") {
+//        let punctArr = userEntry.split(separator: "?")
+//        let numOne = Double(punctArr[0])
+//        let numTwo = Double(punctArr[1])
+//        if let validNumOne = numOne, let validNumTwo = numTwo {
+//            let operation = mathStuffFactory(opString: randomOperator!)
+//            let result = operation(validNumOne, validNumTwo)
+//            print()
+//            print("Answer: \(validNumOne) ? \(validNumTwo) = \(result)")
+//            print()
+//            print("Guess what operator was used in this equation!!!")
+//            let guessOperator = readLine() ?? ""
+//            if guessOperator == randomOperator {
+//                print()
+//                print("nice job")
+//                isCalculating = false
+//            } else {
+//                print("Oops, it was actually \(randomOperator!)")
+//                isCalculating = false
+//            }
+//        } else {
+//            print()
+//            print("For Random Operator... Please enter like so \"x?y\"!!!")
+//        }
+//    } else {
+//        print("Please Enter a Valid Equation")
+//    }
+//
+//    print()
+//    print("would you like to try another calculation")
+//    let calcAgain = readLine() ?? ""
+//    if calcAgain.lowercased() == "yes" {
+//        isCalculating = true
+//    } else if calcAgain.lowercased() == "no" {
+//        isCalculating = false
+//        print("thanks for calculating have a nice day")
+//    } else {
+//        print("Please enter \"yes\" or \"no\"!!!")
+//    }
+//
+//} while isCalculating == true
